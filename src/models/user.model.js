9b1f01f5483d69d281a67c23d1b24368162e831a
Shahi77 <shahiinfinity02@gmail.com>
2024-06-23 23:33:03 +0530

    adding userSchema for authenticatiion

---
+++ src/models/user.model.js
@@ -0,0 +1,26 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+export const USER = mongoose.model("USER", userSchema);
